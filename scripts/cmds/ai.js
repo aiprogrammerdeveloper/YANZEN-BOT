88282d9612ea76ccb751d8b12f1498ab7c416419
Shi-zu-ka-27 <jeanjuniors210@gmail.com>
2024-09-28 21:28:34 +0100

    Create ai.js

---
+++ scripts/cmds/ai.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+
+async function fetchFromAI(url, params) {
+ try {
+ const response = await axios.get(url, { params });
+ return response.data;
+ } catch (error) {
+ console.error(error);
+ return null;
+ }
+}
+
+async function getAIResponse(input, userName, userId, messageID) {
+ const services = [
+ { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+ ];
+
+ let response = ` Bonjour, t'as besoin d'aide..?`;
+ let currentIndex = 0;
+
+ for (let i = 0; i < services.length; i++) {
+ const service = services[currentIndex];
+ const data = await fetchFromAI(service.url, service.params);
+ if (data && (data.gpt4 || data.reply || data.response)) {
+ response = data.gpt4 || data.reply || data.response;
+ break;
+ }
+ currentIndex = (currentIndex + 1) % services.length; // Passer au service suivant
+ }
+
+ return { response, messageID };
+}
+
+module.exports = {
+ config: {
+ name: 'ai',
+ author: 'shizuka',
+ role: 0,
+ aliase: ["ai"],
+ category: 'ai-chat',
+ shortDescription: 'ai to ask anything',
+ },
+ onStart: async function ({ api, event, args }) {
+ const input = args.join(' ').trim();
+ if (!input) {
+ api.sendMessage("Hello..👋How can I assist you today...😁", event.threadID, event.messageID);
+ return;
+ }
+
+ api.getUserInfo(event.senderID, async (err, ret) => {
+ if (err) {
+ console.error(err);
+ return;
+ }
+ const userName = ret[event.senderID].name;
+ const { response, messageID } = await getAIResponse(input, userName, event.senderID, event.messageID);
+ api.sendMessage(`S̲H̲I̲Z̲U̲K̲A̲..B̲O̲T̲..A̲S̲S̲I̲S̲T̲A̲N̲T̲\n━━━━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+ });
+ },
+ onChat: async function ({ api, event, message }) {
+ const messageContent = event.body.trim().toLowerCase();
+ if (messageContent.startsWith("ai")) {
+ const input = messageContent.replace(/^ai\s*/, "").trim();
+ api.getUserInfo(event.senderID, async (err, ret) => {
+ if (err) {
+ console.error(err);
+ return;
+ }
+ const userName = ret[event.senderID].name;
+ const { response, messageID } = await getAIResponse(input, userName, event.senderID, message.messageID);
+ message.reply(`S̲H̲I̲Z̲U̲K̲A̲..B̲O̲T̲..A̲S̲S̲I̲S̲T̲A̲N̲T̲ \n━━━━━━━━━━━━━━━━\n${userName} , ${response} ━━━━━━━━━━━━━━━━\n `, messageID);
+api.setMessageReaction("💬", event.messageID, () => {}, true);
+
+ });
+ }
+ }
+};
